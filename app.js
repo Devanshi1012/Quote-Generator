88d65617fa9ae87b312262469be2860b99c674fe
Devanshi1012 <109812211+Devanshi1012@users.noreply.github.com>
2024-03-03 14:04:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const URL = "https://quote-garden.onrender.com/api/v3/quotes";
+const Quotes = document.querySelector(".Quotes");
+const author = document.querySelector(".Author")
+const btn = document.querySelector("#get");
+// let promise = fetch(URL);
+// console.log(promise);
+const getQuotes = async()=>{
+    let response = await fetch(URL);
+    // console.log(response);
+    let data = await response.json();//readable format
+    // console.log(data);
+    // console.log(data.data[1].
+    //     quoteText);
+    const randomIndex = Math.floor(Math.random() * data.data.length);
+    const randomQuote = data.data[randomIndex]; 
+    // Display the random quote
+    Quotes.innerText = randomQuote.quoteText; 
+    author.innerText = randomQuote.quoteAuthor;
+    
+}
+btn.addEventListener("click",getQuotes);
